8eb5fa50c3f1e24a065f83e60388b8edde845c74
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-03-06 21:33:53 +0200

    [curry] refactor max(..)

--- curry.js
+++ curry.js
@@ -70,6 +70,7 @@ const max = function (xs) {
     xs
   );
 };
+const max = _.reduce(_keepHighest, 0);
 
 QUnit.test("Ex3: max", (assert) => {
   assert.equal(max([323, 523, 554, 123, 5234]), 5234);
